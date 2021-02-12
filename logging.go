9660cc285f5b93118746e4e66b7135cbace91df3
Plankiton <pl4nk1ton@gmail.com>
2021-02-11 23:37:01 -0300

    [Feature] Create logging abstraction for inprove the logging messages

---
+++ logging.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+    "os"
+    "log"
+)
+
+func Log (args ...interface {}) {
+    fmt_args := []interface {}{"\033[32;1m[info] \033[00m"}
+    fmt_args = append(fmt_args, args...)
+
+    log.Println(fmt_args...)
+}
+
+func Err (args ...interface {}) {
+    fmt_args := []interface {}{"\033[31;1m[erro] \033[00m"}
+    fmt_args = append(fmt_args, args...)
+
+    log.Println(fmt_args...)
+}
+
+func War (args ...interface {}) {
+    fmt_args := []interface {}{"\033[33;1m[warn] \033[00m"}
+    fmt_args = append(fmt_args, args...)
+
+    log.Println(fmt_args...)
+}
+
+func Die (args ...interface {}) {
+    fmt_args := []interface {}{"\033[31;1m[erro] \033[00m"}
+    fmt_args = append(fmt_args, args...)
+
+    log.Println(fmt_args...)
+    os.Exit(1)
+}
