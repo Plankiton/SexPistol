2ec71d6d9c6828589512b671291528584f753e4c
Plankiton <pl4nk1ton@gmail.com>
2021-06-03 11:05:16 -0300

    Add html template submodule

--- Html/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Plankiton/SexPistol/Html
-
-go 1.16
--- Html/render.go
+++ Html/render.go
@@ -1,2 +1,27 @@
 package SexHtml
+import (
+    "text/template"
+    "errors"
+    "bytes"
+)
 
+func Render(input interface{}, data interface{}) ([]byte, error) {
+    var err error
+
+    tmpl := template.New("SexTemplate")
+    buffer := new(bytes.Buffer)
+
+    if input, ok := input.(string); ok {
+        tmpl, err = tmpl.Parse(input)
+        if err == nil {
+            err = tmpl.Execute(buffer, data)
+            return buffer.Bytes(), err
+        }
+    }
+
+    if err == nil {
+        err = errors.New("Invalid input type")
+    }
+
+    return []byte{}, err
+}
